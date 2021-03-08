4cfc6bf92d5285fb4d9d0cf99a8be690c80ed0a7
juiceleft88 <juiceleft88@gmail.com>
2021-03-07 23:05:42 -0800

    Finsished

--- Cstringlab.cpp
+++ Cstringlab.cpp
@@ -3,8 +3,8 @@
 
 using namespace std;
 
-int vowels (const char*);
-int consonants(const char*);
+int vowels (const char*); //function prototype to count vowels
+int consonants(const char*); //function prototype to count consonants
 
 int main()
 {
@@ -17,6 +17,7 @@ int main()
 
     do //do-while statement for displaying menu and getting selection input in form of a character
     {
+        //menu of choices
         cout << "Please enter a choice from the following menu:" << endl;
         cout << "A) Count the number of vowels in the string" << endl;
         cout << "B) Count the number of consonants in the string" << endl;
@@ -24,57 +25,63 @@ int main()
         cout << "D) Enter another string" << endl;
         cout << "E) Exit the program " << endl;
         cout << "\nPlease make a selection: " << endl;
-        cin >> selection;
+        cin >> selection; //getting the single character selection
         cout << "\n";
 
-        switch (toupper(selection))
+        switch (toupper(selection)) //to not have so much data validation, just converted char to capital automatically here. 
+        //to perform following actions based on the selection from user.
         {
-            case 'A' : cout << vowels(input) << " vowels are in this string." << endl;
+            case 'A' : cout << vowels(input) << " vowels are in this string." << endl; //to count vowels
             break;
 
-            case 'B' : cout << consonants(input) << " consonants are in this string." << endl;
-            break;
-        
-            case 'C': cout << vowels(input) << " vowels and " << consonants(input) << " consonants are in this string." << endl;
+            case 'B' : cout << consonants(input) << " consonants are in this string." << endl; //to count consonants
             break;
 
-            case 'D': cout << "Enter string of no more than 49 characters: ";
-                  cin.ignore();
-                  cin.getline(input, 50);
+            //to count consonants and vowels, wasnt sure if they had to bee added into one number or displayed separately so did both
+            case 'C': cout << vowels(input) << " vowels and " << consonants(input) << " consonants are in this string, " << "for a total of " << vowels(input) + consonants(input) << endl;
+            break;
+
+            case 'D': cout << "Enter string of no more than 49 characters: " << endl; // to enter another string
+                  cin.ignore(); //to clear previous cin.getline
+                  cin.getline(input, 50); //input
+                  cout << "\n";
                   break;
-        
+
+            //no action to be taken here if E is chosen
             case 'E' : break;
-            default : cout << "Please enter a valid choice of A, B , C, D or E. ";
+            //using default statement here for input validation
+            default : cout << "\nPlease enter a valid choice of A, B , C, D or E. ";
         }
 
-    } while (toupper(selection) !='E');
+    } while (toupper(selection) !='E'); //end of do-while statament. Repeats the 'do' portion so long as 'E' is not a selection
 
-    if (toupper(selection) == 'E')
+    if (toupper(selection) == 'E') //ends program if 'E' is selected
     return 0;
 }
 
-int vowels(const char *x )
+int vowels(const char *x ) //function to count the vowels, using pointer as parameter
 {
-    int counter = 0;
+    int counter = 0; //variable to hold the count of vowels
 
-    while (*(x) != '\0')
+    while (*(x) != '\0') //while x is not at the end of the string
     {
+        // loop to count the vowels
         if (toupper(*x) == 'A' || toupper(*x) == 'E' || toupper(*x) == 'I' || toupper(*x) == 'O'
             || toupper(*x) == 'U' || toupper(*x) == 'Y')
-            counter++;
+            counter++; //keeps tally of vowels
             x++;
     }
     return counter;
 }
 
-int consonants(const char *x)
+int consonants(const char *x) //same idea as above function, except while statement excludes vowels, and counts the rest of string letters
 {
-    int counter = 0;
+    int counter = 0; //variable to keep count
 
-    while (*(x) != '\0')
+    while (*(x) != '\0') //while x is not at end of string
     {
         if (toupper(*x) != 'A' && toupper(*x) != 'E' && toupper(*x) != 'I' && toupper(*x) != 'O'
-            && toupper(*x) != 'U' && toupper(*x) != 'Y')
+            && toupper(*x) != 'U' && toupper(*x) != 'Y' && toupper(*x) != ' ' && !ispunct(*x) && !isdigit(*x)) //!isdigit and !ispunct to not include any digits or punctuation as consonant count and ' ' to not count empty spaces
             counter++;
             x++;
     }
