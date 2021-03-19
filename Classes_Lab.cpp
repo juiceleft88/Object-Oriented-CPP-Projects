b3897e4d20656469b92b1843b4522daa04b616ea
juiceleft88 <juiceleft88@gmail.com>
2021-03-19 07:18:10 -0700

    framework

---
+++ Classes_Lab.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <iomanip>
+#include <ostream>
+#include <string>
+
+using namespace std;
+
+class Trivia
+{
+    public:
+    Trivia();
+    
+
+    private:
+    string question;
+    int answer1;
+    int answer2;
+    int answer3;
+    int answer4;
+    int correctAnswer;
+
+};
+
+int main()
+{
+
+    return 0;
+}
