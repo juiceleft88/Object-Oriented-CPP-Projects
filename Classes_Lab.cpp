72b02392667b1decc09d8ac33e63633ac4933691
juiceleft88 <juiceleft88@gmail.com>
2021-03-19 13:32:41 -0700

    formatting

--- Classes_Lab.cpp
+++ Classes_Lab.cpp
@@ -8,7 +8,7 @@ using namespace std;
 class Question
 {
     public:
-    Question();
+    Question(){};
     void setQuestion(string q)
         { question = q;}
     void setAnswer1(string ans1)
@@ -19,10 +19,10 @@ class Question
         { answer3 = ans3;}
     void setAnswer4(string ans4)
         { answer4 = ans4;}
-    void setcorrectAnswer(int corr)
+    void setcorrectAnswer(string corr)
         {correctAnswer = corr;}
     string getQuestion()
-        {return q;}
+        {return question;}
     string getAnswer1()
         {return answer1;}
     string getAnswer2()
@@ -31,8 +31,9 @@ class Question
         {return answer3;}
     string getAnswer4()
         {return answer4;}
-    int getAnswer()
+    string getAnswer()
         {return correctAnswer;}
+    void displayQuestion();
  
 
     private:
@@ -41,54 +42,101 @@ class Question
     string answer2;
     string answer3;
     string answer4;
-    int correctAnswer;
+    string correctAnswer;
 
 };
-
-Question::Question()
-{
-
-}
-
-void Question::displayQuestion()
-{
-    string input;
-
-    fstream nameFile;
-    nameFile.open("Trivia.txt", ios::in);
-    if (nameFile)
-    {
-        for (int j = 0; j <10; j++)
-        {
-        for (int i=0; (i+5)<6; i++)
-        {
-            getline(nameFile, input);
-            cout << input << endl;
-        }
-        }
-
-        nameFile.close();
-    }
-    else
-    {
-        cout << "Error: cannot open file" << endl;
-    }
-
-}
-
 
 
 int main()
 {
     cout << "Welcome to Trivia Night! This is a game for two players who will compete to answer the most questions correctly.\n";
     cout << "The rules of the game are as follows: \n" << "-There are a total of 10 questions, of which each player will answer five. \n";
-    cout << "-With each question, there will be four possible answer choices to choose from. \n";
-    cout << "-Points will be added for every correct answer, and tallied to see who wins at the end. \n";
+    cout << "- With each question, there will be four possible answer choices to choose from. \n";
+    cout << "- Points will be added for every correct answer, and tallied to see who wins at the end. \n";
+    cout << "- Please enter the digit (1-4) that corresponds with your answer choice:"
     cout << "\n Let the game begin! \n";
 
-    Question game;
-    game.displayQuestion();
+    ifstream trivia;
+    int player1score = 0, player2score = 0;
+    string answer;
+    string quest, opt1, opt2, opt3, opt4, correct;
 
+    trivia.open("Trivia.txt");
 
+    Question game[10];
+        for (int index=0; index<10; index++)
+        {
+            if (index%2 == 0)
+            {
+                cout << "Question for Player 1: \n ";
+                getline(trivia, quest);
+                getline(trivia, opt1);
+                getline(trivia, opt2);
+                getline(trivia, opt3);
+                getline(trivia, opt4);
+                getline(trivia, correct);
+                game[index].setQuestion(quest);
+                game[index].setAnswer1(opt1);
+                game[index].setAnswer2(opt2);
+                game[index].setAnswer3(opt3);
+                game[index].setAnswer4(opt4);
+                game[index].setcorrectAnswer(correct);
+
+                cout << game[index].getQuestion() << endl;
+                cout << "1. " << game[index].getAnswer1() << endl;
+                cout << "2. " << game[index].getAnswer2() << endl;
+                cout << "3. " << game[index].getAnswer3() << endl;
+                cout << "4. " << game[index].getAnswer4() << endl;
+                cin >> answer;
+                    if (game[index].getAnswer() == answer)
+                    {
+                    cout << "\nYour answer is correct! \n";
+                    player1score++;
+                    }
+                    else cout << "That answer is....incorrect."
+                    
+            }
+            else if(index%2 == 1)
+            {
+                cout << "\nQuestion for Player 2: \n";
+                getline(trivia, quest);
+                getline(trivia, opt1);
+                getline(trivia, opt2);
+                getline(trivia, opt3);
+                getline(trivia, opt4);
+                getline(trivia, correct);
+                game[index].setQuestion(quest);
+                game[index].setAnswer1(opt1);
+                game[index].setAnswer2(opt2);
+                game[index].setAnswer3(opt3);
+                game[index].setAnswer4(opt4);
+                game[index].setcorrectAnswer(correct);
+
+                cout << game[index].getQuestion() << endl;
+                cout << "1. " << game[index].getAnswer1() << endl;
+                cout << "2. " << game[index].getAnswer2() << endl;
+                cout << "3. " << game[index].getAnswer3() << endl;
+                cout << "4. " << game[index].getAnswer4() << endl;
+                cin >> answer;
+                    if (game[index].getAnswer() == answer)
+                    {
+                    cout << "\nYour answer is correct! \n";
+                    player2score++;
+                    }
+                    else cout << "That answer is....incorrect."
+
+            }
+
+        }
+        trivia.close();
+        cout << "\nPlayer 1 scored: " << player1score << " points." << endl;
+        cout << "Player 2 scored: " << player2score << " points.\n\n" << endl;
+        
+        if (player1score > player2score)
+            cout << "Player 1 is the winner!" << endl;
+        else if (player1score < player2score)
+            cout << "Player 2 is the winner!" << endl;
+        else if (player1score == player2score)
+            cout << "It's a tie! Play again!";
     return 0;
 }
