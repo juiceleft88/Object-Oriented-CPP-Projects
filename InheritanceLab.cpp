2b9ed9dea7f4fccbdde0947f8989732a330742a5
juiceleft88 <juiceleft88@gmail.com>
2021-04-22 15:05:57 -0700

    Functioning program

--- InheritanceLab.cpp
+++ InheritanceLab.cpp
@@ -1,70 +1,101 @@
+//Hugo Izquierdo
+//886895301
+//The purpose of this program is to creat a virtual function to calculate the area of the derived classes
+
 #include <iostream>
 #include <iomanip>
 
 using namespace std;
 
-class BasicShape
+class BasicShape //base class abstract type
 {
     private:
-    double area;
+    double area; 
 
     public:
-    double getArea()
+    double getArea() //accessor 
         {return area;}
-    virtual void calcArea()=0;
-    void setArea(int ar)
+    virtual void calcArea()=0; //virtual function for calculating area. Does not contain data or a body.
+    void setArea(double ar) // mutator function to set the area, and thus access the private member
         {area = ar;}
 };
 
-class Circle : public BasicShape
+class Circle : public BasicShape //derived class Circle from the the BasicChild class
 {
-    private:
+    private: //private members for this class
     long centerX;
     long centerY;
     double radius;
 
     public:
-    Circle()
+    Circle() // default constructor
     {
         centerX = 0;
         centerY = 0;
         radius = 0.0;
     }
-    Circle(long xval, long yval, double r)
+    Circle(long xval, long yval, double r) //parametrized constructor, which calls the overriden calcArea function
     { 
         centerX = xval;
         centerY = yval;
         radius = r;
+        calcArea();
+        
     }
-    long getCenterX()
+    long getCenterX() //Accessor
         {return centerX;}
-    long getCenterY()
+    long getCenterY() //Accessor
         {return centerY;}
-    void calcArea()
+    void calcArea()  //overriden virtual function
     {
         double circArea;
         circArea = 3.14159 * radius * radius;
-        cout << circArea;
+        setArea(circArea); //calling setArea function from base class, and passing local variable circArea as an argument. This sets circArea to 'area'
     }
 };
 
-class Rectangle : public BasicShape
+class Rectangle : public BasicShape //deived class Rectangle from BasicShape base class
 {
-    private:
+    private: 
     long width;
     long length;
 
     public:
-    long getWidth();
-    long getLength();
-    void calcArea();
+    Rectangle()
+    {
+        width = 0;
+        length = 0;
+    }
+    Rectangle(long w, long l) //parametrized constructor
+    {
+        width = w;
+        length = l;
+        calcArea(); //calling calcArea function in constructor
+    }
+    long getWidth() //accessors
+        {return width;}
+    long getLength()
+        {return length;}
+    void calcArea() //overriden virtual function from base class. Used to claculate the rectangle's area.
+    {
+        double rectArea;
+        rectArea = length * width;
+        setArea(rectArea); //calling setArea function from base class, and passing local variable rectArea as an argument. This sets rectArea to 'area'
+    }
 };
 
 
 int main()
 {
     Circle circ(5, 5, 1.0);
-    circ.calcArea();
+    cout << "The area of the circle is: ";
+    cout << circ.getArea() << " units.";
+
+    cout << "\nThe are of the square is: ";
+
+    Rectangle rect(4, 3);
+    
+    cout << rect.getArea() << " units.";
 
     return 0;
 }
