3429e9974e51b65b20172c79ed7142754d71e54c
juiceleft88 <70562965+juiceleft88@users.noreply.github.com>
2023-02-01 16:23:41 -0800

    Create spellCheckMain.cpp

---
+++ SpellCheckerProg/spellCheckMain.cpp
@@ -0,0 +1,19 @@
+#include "spellcheck.hpp"
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <vector>
+using namespace std;
+
+int main()
+{
+    std::string target = "progrum "; // enter a different word in quotations to test
+    SpellCheck check; // to be abe to call ifstream actions
+    cout << "Spell Check " << endl;
+    std::ifstream infile ("words.txt"); // "words.txt is name of my dictionary"
+    if (!infile.is_open()) throw std::exception();
+    check.load_dictionary(infile);
+    check.find(target);
+    infile.close();
+    check.suggest(target, 3); // gives three suggestions
+};
